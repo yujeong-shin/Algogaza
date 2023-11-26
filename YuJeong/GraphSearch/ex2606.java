5683333cecad91ed7f12de6e01681d4c5a2bd471
yujeong-shin <vouobb@naver.com>
2023-11-26 16:32:27 +0900

    백준 2606문제 풀이 - 인접리스트&DFS

--- YuJeong/GraphSearch/ex2606.java
+++ YuJeong/GraphSearch/ex2606.java
@@ -1,24 +1,16 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ex2606 {
     static int N, E, count;
     static int[] ch;
-    static int[][] graph;
-    public static void BFS(int v) {
-        Queue<Integer> Q = new LinkedList<>();
-        ch[v]=1;
-        Q.offer(v);
-
-        while(!Q.isEmpty()) {
-            int tmp = Q.poll();
-            for(int j=0; j<N+1; j++) {
-                if(graph[tmp][j]==1 && ch[j]==0) {
-                    count++;
-                    ch[j]=1;
-                    Q.offer(j);
-                }
+    static ArrayList<ArrayList<Integer>> graph;
+    public static void DFS(int v){
+        for(int nv : graph.get(v)) {
+            if(ch[nv]==0) {
+                ch[nv]=1;
+                count++;
+                DFS(nv);
             }
         }
     }
@@ -26,15 +18,20 @@ public class ex2606 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        graph = new int[N+1][N+1];
+        graph = new ArrayList<>();
+        for(int i=0; i<N+1; i++) {
+            graph.add(new ArrayList<>());
+        }
         ch = new int[N+1];
         E = sc.nextInt();
         for(int i=0; i<E; i++) {
             int s = sc.nextInt();
             int e = sc.nextInt();
-            graph[s][e] = graph[e][s] = 1;
+            graph.get(s).add(e);
+            graph.get(e).add(s);
         }
-        ex2606.BFS(1);
+        ch[1]=1;
+        ex2606.DFS(1);
         System.out.println(count);
     }
 }
