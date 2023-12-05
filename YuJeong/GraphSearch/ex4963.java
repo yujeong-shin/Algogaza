92e1eb3efac00601ad295f8e126d183f90d21039
yujeong-shin <vouobb@naver.com>
2023-12-05 14:28:59 +0900

    백준 4963문제 풀이 - BFS

--- YuJeong/GraphSearch/ex4963.java
+++ YuJeong/GraphSearch/ex4963.java
@@ -1,10 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
+class Point1{
+    int x, y;
+    public Point1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 public class ex4963 {
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
@@ -38,7 +43,8 @@ public class ex4963 {
                 for (int j = 0; j < w; j++) {
                     if(graph[i][j]==1) {
                         count++;
-                        DFS(i, j);
+                        //DFS(i, j);
+                        BFS(i, j);
                     }
                 }
             }
@@ -55,6 +61,21 @@ public class ex4963 {
             int ny = y + dy[i];
             if(nx>=0 && nx<h && ny>=0 && ny<w && graph[nx][ny]==1){
                 DFS(nx, ny);
+            }
+        }
+    }
+    public static void BFS(int x, int y) {
+        Queue<Point1> Q = new LinkedList<>();
+        Q.offer(new Point1(x, y));
+        while(!Q.isEmpty()) {
+            Point1 cur = Q.poll();
+            for(int i=0; i<8; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if(nx>=0 && nx<h && ny>=0 && ny<w && graph[nx][ny]==1){
+                    graph[nx][ny]=0;
+                    Q.offer(new Point1(nx, ny));
+                }
             }
         }
     }
