8e9fa43bd759ec5700b80d206d3483bbf9dd2135
SonJM <blisle@naver.com>
2023-12-12 10:05:37 +0900

    Q2206 메모리 초과

--- JJEONGMIN/week2/Q2206.java
+++ JJEONGMIN/week2/Q2206.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Q2206 {
-    static int[][] map;
+    static int[][] map, map2;
     static int[][][] count;
     static ArrayList<Integer> result;
     static boolean[][] visited;
@@ -22,6 +23,8 @@ public class Q2206 {
         W = Integer.parseInt(st.nextToken());
 
         map = new int[H][W];
+        map2 = new int[H][W];
+
         for(int i=0; i<H; i++){
             String str = br.readLine();
             for(int j=0; j<W; j++) {
@@ -29,25 +32,39 @@ public class Q2206 {
                 if(map[i][j] == 1) walls++;
             }
         }
+
+        for(int i=0; i<map.length; i++){
+            map2[i] = map[i].clone();
+        }
+
         count = new int[walls][H][W];
         visited = new boolean[H][W];
         is_crash = new boolean[walls];
 
         bfs(0,0);
         Collections.sort(result);
-        System.out.println(result.getFirst());
+        while(result.remove(Integer.valueOf(0)));
 
+        if(result.isEmpty()) System.out.println(-1);
+        else System.out.println(result.get(0));
     }
     private static void bfs(int start_H, int start_W){
         Queue queue = new LinkedList<Integer>();
-        queue.offer(new Point(start_H, start_W));
 
-        while(!queue.isEmpty()){
-            Point p = (Point) queue.poll();
-            if(p.x == H-1 && p.y == W-1) break;
+        for(int i=0; i<walls; i++)
+        {
+            queue.offer(new Point(start_H, start_W));
+            count[i][0][0] = 1;
+            is_crash[i] = false;
 
-            for(int i=0; i<walls; i++){
-                count[i][0][0] = 1;
+            for(int k=0; k>map.length; k++){
+                map[i] = map2[i].clone();
+            }
+
+            while(!queue.isEmpty()){
+                Point p = (Point) queue.poll();
+                if(p.x == H-1 && p.y == W-1) break;
+
                 for(int j=0; j<4; j++) {
                     int next_H = p.x + dy[j];
                     int next_W = p.y + dx[j];
@@ -57,19 +74,18 @@ public class Q2206 {
                         else if(map[next_H][next_W] == 1 && !is_crash[i] && !visited[next_H][next_W]){
                             is_crash[i] = true;
                             visited[next_H][next_W] = true;
-                            queue.offer(new Point(next_H, next_W));
                             count[i][next_H][next_W] = count[i][p.x][p.y] + 1;
-                            break;
+                            queue.offer(new Point(next_H, next_W));
                         }
-                        else if(map[next_H][next_W] == 0){
+                        else if(map[next_H][next_W] == 0 && !visited[next_H][next_W]){
+                            map[next_H][next_W] = 1;
                             queue.offer(new Point(next_H,next_W));
                             count[i][next_H][next_W] = count[i][p.x][p.y] + 1;
-                            break;
                         }
                     }
                 }
-                result.add(count[i][H-1][W-1]);
             }
+            result.add(count[i][H-1][W-1]);
         }
     }
 }
