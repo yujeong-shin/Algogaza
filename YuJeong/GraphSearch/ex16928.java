867c7d124ebfcde65d0a92d35b1a537e1e167c8e
yujeong-shin <vouobb@naver.com>
2023-12-19 14:23:50 +0900

    백준 16928문제 풀이 - 반례 찾아줄 사람

--- YuJeong/GraphSearch/ex16928.java
+++ YuJeong/GraphSearch/ex16928.java
@@ -14,7 +14,7 @@ public class ex16928 {
         ladderCount = Integer.parseInt(st.nextToken());
         snakeCount = Integer.parseInt(st.nextToken());
         map = new HashMap<>();
-        for (int i = 0; i <ladderCount ; i++) {
+        for (int i = 0; i <ladderCount; i++) {
             st = new StringTokenizer(bf.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
@@ -35,20 +35,25 @@ public class ex16928 {
     // 100까지 도착하기 위해 굴려야 하는 주사위의 횟수는?
     static void BFS(int num){
         Queue<Integer> Q = new LinkedList<>();
+        boolean[] visited = new boolean[101];
         Q.add(num);
         int level=0;
         while (!Q.isEmpty()){
             int len = Q.size();
             for (int i = 0; i < len; i++) {
                 int now = Q.poll();
-                if(now==100) {
-                    answer=level;
+                //94~99에서는 다음번에 무조건 100에 도달할 수 있음
+                if(now>=94 && now<=99) {
+                    //반복 중간에 찾아지면 한번 더 이동해야 100이 되기 때문에 +1
+                    answer=++level;
                     return;
                 }
-                else {
-                    for (int j = 1; j <= 6; j++) {
-                        int next = now+j;
+                for (int diceNum = 1; diceNum <= 6; diceNum++) {
+                    int next = now + diceNum;
+                    if(!visited[next] && next<=100) {
+                        visited[next]=true;
                         if(map.containsKey(next)) Q.add(map.get(next));
+                        Q.add(next);
                     }
                 }
             }
