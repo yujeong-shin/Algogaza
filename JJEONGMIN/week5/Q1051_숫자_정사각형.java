aad36213dae1c9a367b7470ca17fdef40ac1853f
SonJM <blisle@naver.com>
2023-12-27 17:53:10 +0900

    뷁

--- JJEONGMIN/week5/Q1051_숫자_정사각형.java
+++ JJEONGMIN/week5/Q1051_숫자_정사각형.java
@@ -1,2 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Q1051_숫자_정사각형 {
+    static char[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            for(int j=0; j<N; j++){
+
+            }
+        }
+    }
+    private static boolean isSquare(){
+        return false;
+    }
 }
