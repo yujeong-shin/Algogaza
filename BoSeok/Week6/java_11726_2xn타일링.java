a81a1cdd18f7048bf96f619452b05d559ce76b7a
boseok <fa7271@naver.com>
2024-01-03 00:06:27 +0900

    240102 java_11726_2xn타일링 index에러 안나게 좀 문제내주시면 너무 좋겠는데 어림도없겠지

--- BoSeok/Week6/java_11726_2xn타일링.java
+++ BoSeok/Week6/java_11726_2xn타일링.java
@@ -3,17 +3,24 @@ package BoSeok.Week6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.stream.IntStream;
+
+import static java.lang.System.exit;
 
 public class java_11726_2xn타일링 {
     static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        dp = new int[n + 1];
+        dp = new int[n + 2];
         dp[1] = 1 ;
         dp[2] = 2 ;
-        IntStream.rangeClosed(3, n).forEach(i -> dp[i] = (dp[i - 1] + dp[i - 2])%10007);
+        if (n <= 2) {
+            System.out.println(dp[n]);
+            exit(0);
+        }
+        IntStream.range(3, n+1).forEach(i -> dp[i] = (dp[i - 1] + dp[i - 2])%10007);
         System.out.println(dp[n]);
     }
 }
