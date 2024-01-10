e9e427ddd9eacdabf02458382e134fd837118a09
SonJM <blisle@naver.com>
2024-01-10 22:41:33 +0900

    계란뿌셔 지구뿌셔 팝핀뿌셔

--- JJEONGMIN/week7/Q16987_계란으로_계란을치면_계란이깨진다.java
+++ JJEONGMIN/week7/Q16987_계란으로_계란을치면_계란이깨진다.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -10,16 +9,13 @@ public class Q16987_계란으로_계란을치면_계란이깨진다 {
     static class eggMayo {
         int durability;
         int weight;
-        boolean boom;
         public eggMayo(int durability, int weight){
             this.durability = durability;
             this.weight = weight;
-            this.boom = false;
         }
     }
     static List<eggMayo> eggMayoList = new ArrayList<>();
-    static boolean[] visited;
-    static int answer = 0, count = 0;
+    static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -33,40 +29,34 @@ public class Q16987_계란으로_계란을치면_계란이깨진다 {
                     )
             );
         }
-        visited = new boolean[N];
-        for(int i=0; i<eggMayoList.size(); i++){
-            visited[i] = true;
-            dfs(i);
-        }
+        dfs(0, 0);
 
         System.out.println(answer);
     }
-    private static void dfs(int current){
-        int boom_count = 0;
-        for(eggMayo egg : eggMayoList){
-            if(egg.boom) boom_count++;
-            answer = Math.max(answer, boom_count);
+    private static void dfs(int current, int count){
+        if(current == eggMayoList.size()){
+            answer = Math.max(answer, count);
+            return;
         }
-        if(current == eggMayoList.size()) return;
+        if(eggMayoList.get(current).durability<=0 || count == eggMayoList.size()-1){
+            dfs(current+1, count);
+            return;
+        }
 
+        int bfCount = count;
         for(int i=0; i<eggMayoList.size(); i++){
-            if(eggMayoList.get(current).boom) break;
-            if(i != current && !eggMayoList.get(i).boom && !visited[i]){
-                visited[i] = true;
-                eggMayoList.get(current).durability -= eggMayoList.get(i).weight;
-                eggMayoList.get(i).durability -= eggMayoList.get(current).weight;
-                if(eggMayoList.get(current).durability < 0) {
-                    eggMayoList.get(current).boom = true;
-                    break;
-                }
-                if(eggMayoList.get(i).durability < 0) eggMayoList.get(i).boom = true;
-                dfs(current+1);
-                visited[i] = false;
-                eggMayoList.get(current).durability += eggMayoList.get(i).weight;
-                eggMayoList.get(i).durability += eggMayoList.get(current).weight;
-                if(eggMayoList.get(current).durability > 0) eggMayoList.get(current).boom = true;
-                if(eggMayoList.get(i).durability > 0) eggMayoList.get(i).boom = false;
-            }
+            if(i == current) continue;
+            if(eggMayoList.get(i).durability<=0) continue;
+
+            eggMayoList.get(current).durability -= eggMayoList.get(i).weight;
+            eggMayoList.get(i).durability -= eggMayoList.get(current).weight;
+            if(eggMayoList.get(current).durability <= 0) count++;
+            if(eggMayoList.get(i).durability <= 0) count++;
+            dfs(current+1, count);
+            eggMayoList.get(current).durability += eggMayoList.get(i).weight;
+            eggMayoList.get(i).durability += eggMayoList.get(current).weight;
+            count = bfCount;
         }
     }
 }
+
