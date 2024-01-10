17e998d90d69b496c14ff18199a128fa8d8ff5d3
yujeong-shin <vouobb@naver.com>
2024-01-10 23:33:52 +0900

    백준 2293, 2578문제 복습 - 코드 루팡

--- YuJeong/Implementation/ex2578_빙고.java
+++ YuJeong/Implementation/ex2578_빙고.java
@@ -1,70 +1,97 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
-//class Point{
-//    int x;
-//    int y;
-//    public Point(int x, int y) {
-//        this.x = x;
-//        this.y = y;
-//    }
-//}
+import static java.lang.System.exit;
+
 public class ex2578_빙고 {
+    static int[][] bingo;
+    static int count ;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        Map<Integer, Point> map = new HashMap<>();
-        int n = 5;
-        int number_count=0;
-        int total_count=0;
-        int[] bingoRow = new int[n];
-        int[] bingoColumn = new int[n];
-        int[] bingoX = new int[2];
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                //map에 "숫자"-"(x좌표,y좌표)" 쌍으로 집어넣음
-                map.put(Integer.parseInt(st.nextToken()), new Point(i, j));
+        bingo = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer stk = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < 5; j++) {
+                bingo[i][j] = Integer.parseInt(stk.nextToken());
             }
         }
-//        원하는대로 잘 들어갔네?
-//        Set<Integer> keySet = map.keySet();
-//        for (Integer key : keySet) {
-//            System.out.println(key + " : " + map.get(key).x + ", " + map.get(key).y);
-//        }
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                // 사회자의 입력값 하나씩 받자
-                int input_number = Integer.parseInt(st.nextToken());
-                System.out.println("input_number = " + input_number);
-                number_count++;
-                System.out.println("number_count = " + number_count);
-
-                Point point = map.get(input_number);
-                System.out.println("사회자가 부른 숫자에 대한 좌표값 : " +  point.x + ", " + point.y);
-
-                bingoRow[point.x]++; //가로 빙고 count
-                System.out.println("point.x : " + point.x + ", bingoRow[point.x] : " + bingoRow[point.x]);
-                if(bingoRow[point.x]==n) total_count++;
-                System.out.println("total_count = " + total_count);
-
-                bingoColumn[point.y]++; //세로 빙고 count
-                System.out.println("point.y : " + point.y +", bingoColumn[point.y] : " + bingoColumn[point.y]);
-                if( bingoColumn[point.y]==n) total_count++;
-                System.out.println("total_count = " + total_count);
-
-                if(point.x == point.y) bingoX[0]++; //우하향 대각선에 있는 값들
-                if(bingoX[0]==n) total_count++;
-                if(point.y == n-(point.x)+1) bingoX[1]++; //좌하향 대각선에 있는 값들
-                if(bingoX[1]==n) total_count++;
-
-                if(total_count==3) System.out.println(number_count);
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer stk = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < 5; j++) {
+                int target = Integer.parseInt(stk.nextToken());
+                for (int k = 0; k < 5; k++) {
+                    for (int l = 0; l < 5; l++) {
+                        if (bingo[k][l] == target) {
+                            bingo[k][l] = 0;
+                        }
+                    }
+                }
+                rCheck();
+                cCheck();
+                lDownCheck();
+                rDownCheck();
+                if (count >= 3) { // 3줄 이상 빙고이면 몇 번째 숫자인지 출력하고 종료
+                    System.out.println(i*5 + j + 1);
+                    exit(0);
+                }
+                count = 0;
             }
         }
+    }
+
+
+    //가로 체크
+    public static void rCheck() {
+        for (int i = 0; i < 5; i++) {
+            int zeroCount = 0;
+            for (int j = 0; j < 5; j++) {
+                if (bingo[i][j] == 0)
+                    zeroCount++;
+            }
+            if (zeroCount == 5)
+                count++;
+        }
+    }
+
+    // 세로 체크
+    public static void cCheck() {
+        for (int i = 0; i < 5; i++) {
+            int zeroCount = 0;
+            for (int j = 0; j < 5; j++) {
+                if (bingo[j][i] == 0)
+                    zeroCount++;
+            }
+            if (zeroCount == 5)
+                count++;
+        }
+    }
+
+    // 우 하향대각선
+    public static void lDownCheck() {
+        int zeroCount = 0;
+        for (int i = 0; i < 5; i++) {
+            if (bingo[i][i] == 0)
+                zeroCount++;
+        }
+        if (zeroCount == 5)
+            count++;
+    }
+
+    // 우상향대각선
+    public static void rDownCheck() {
+        int zeroCount = 0;
+        for (int i = 0; i < 5; i++) {
+            if (bingo[i][4 - i] == 0)
+                zeroCount++;
+        }
+        if (zeroCount == 5)
+            count++;
     }
 }
