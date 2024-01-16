aec7b138158aab904c5a50babacf81d04e77b287
qoth-0 <bny1324@naver.com>
2024-01-16 18:12:14 +0900

    week7

---
+++ SoYoung/week7/B_15649.java
@@ -0,0 +1,46 @@
+// N과 M (1)
+
+import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static List<Integer> list = new ArrayList<>();
+    static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        check = new boolean[n+1];
+
+        seq(n, m);
+    }
+    static void seq(int n, int m) throws IOException {
+        if(list.size() == m) {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            for(int a : list) {
+                bw.write(a + " ");
+            }
+            bw.write("\n");
+            bw.flush();
+        }
+
+        for(int i=1; i<=n; i++) {
+            if(!check[i]) {
+                check[i] = true;
+                list.add(i);
+                seq(n, m);
+                list.remove(list.size()-1);
+                check[i] = false;
+            }
+        }
+
+    }
+}
