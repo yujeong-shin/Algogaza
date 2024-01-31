0c2673d91ae71bf8e3910ccdf301c6e82c05de29
yujeong-shin <vouobb@naver.com>
2024-02-01 00:21:30 +0900

    N과M 시리즈 7~12 풀기

---
+++ YuJeong/BackTracking/ex15665_N과M_11.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class ex15665_N과M_11 {
+    static int n, m;
+    static int[] num, combi;
+    static Set<String> set;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        num = new int[n];
+        combi = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        set = new LinkedHashSet<>();
+
+        DFS(0);
+        set.forEach(System.out::println);
+    }
+
+    static void DFS(int L){
+        if(L==m){
+            String temp = "";
+            for(int number : combi){
+                temp += number + " ";
+            }
+            set.add(temp);
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                combi[L] = num[i];
+                DFS(L+1);
+            }
+        }
+    }
+}
