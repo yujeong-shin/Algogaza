70aa73683adfce0974fdb5fa6350cc3181448d9c
SonJM <blisle@naver.com>
2024-02-28 17:52:11 +0900

    카잉 달력,,,

--- JJEONGMIN/week11/Q6064_카잉_달력.java
+++ JJEONGMIN/week11/Q6064_카잉_달력.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,20 +8,43 @@ public class Q6064_카잉_달력 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(st.nextToken());
         while(T-- > 0){
             st = new StringTokenizer(br.readLine());
-            int M = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            Point cal1 = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            Point cal2 = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
-            int result = kaing(M,N,x,y);
-            System.out.println(result);
+            int result = kaing(cal1, cal2);
+
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
+    private static int kaing(Point kaingCal, Point resultCal){
+        int diff = Math.abs(kaingCal.x - kaingCal.y);
+        int kaingMinValue = Math.min(kaingCal.x, kaingCal.y);
+        int kaingMaxValue = Math.max(kaingCal.x, kaingCal.y);
+        int resultDiff = Math.abs(resultCal.x- resultCal.y);
+        int result = 0, count = 0;
+        if(!isPossible(diff, kaingMinValue, resultDiff)) return -1;
+
+        for(int i=kaingMinValue; i > 0; i -= diff){
+            if(i == resultDiff) break;
+            count++;
+        }
+
+        if(resultCal.x < resultCal.y){
+            return (count * kaingMinValue) + (resultCal.x - 1);
+        } else{
+            return -1;
         }
     }
-    private static int kaing(int M, int N, int x, int y){
-        return 0;
+    private static boolean isPossible(int diff, int kaingMin, int resultDiff){
+        for(int i=kaingMin; i > 0; i -= diff){
+            if(resultDiff == i) return true;
+        }
+        return false;
     }
 }
